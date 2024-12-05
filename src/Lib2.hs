5e072350792dc3e25157f1e75c2808057d1292cd
Ignė Balvočiūtė <142167949+ignebal@users.noreply.github.com>
2024-12-05 16:23:55 +0200

    Lab 2

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -1,44 +1,161 @@
-{-# LANGUAGE InstanceSigs #-}
-module Lib2
-    ( Query(..),
+{-# LANGUAGE StrictData #-}
+
+module Lib2 (
+    Query(..),
     parseQuery,
     State(..),
     emptyState,
-    stateTransition
-    ) where
+    stateTransition,
+    Song(..),
+    parseSong,
+    parseSongList
+) where
 
--- | An entity which represets user input.
--- It should match the grammar from Laboratory work #1.
--- Currently it has no constructors but you can introduce
--- as many as needed.
+import Data.Char (isDigit, isSpace)
+
+-- Song data type
+data Song = Song
+    { songTitle :: String,
+        songSinger :: String,
+        songGenre :: String,
+        songLength :: (Int, Int)
+    }
+    deriving (Show, Eq)
+
+-- User command data type
 data Query
+    = OpenSongList [Song]
+    | LikeSong Song
+    | UnlikeSong Song
+    deriving (Show, Eq)
 
--- | The instances are needed basically for tests
-instance Eq Query where
-  (==) _ _= False
+-- Parsing Helpers
+orElse :: Either String a -> Either String a -> Either String a
+orElse (Right x) _ = Right x
+orElse (Left _) y = y
 
-instance Show Query where
-  show _ = ""
+-- Basic Parsers
+digit :: String -> Either String Char
+digit str = case str of
+    (x:_) | isDigit x -> Right x
+    _ -> Left "Expected a digit"
 
--- | Parses user's input.
--- The function must have tests.
+string :: String -> String -> Either String String
+string expected actual =
+    if take (length expected) actual == expected
+        then Right (drop (length expected) actual) -- Consume the matched string!
+        else Left ("Expected \"" ++ expected ++ "\"")
+
+parseTwoDigit :: String -> Either String Int
+parseTwoDigit str =
+    if length str /= 2
+        then Left "Expected two digits"
+        else case (digit (take 1 str), digit (take 1 (drop 1 str))) of
+            (Right d1, Right d2) -> case reads [d1, d2] :: [(Int, String)] of
+                [(num, "")] -> Right num
+                _ -> Left "Invalid number"
+            (Left err, _) -> Left err
+            (_, Left err) -> Left err
+
+parseLength :: String -> Either String (Int, Int)
+parseLength str =
+    let parseMMSS inputStr = case break (== ':') inputStr of
+            (minutesStr, ':':secondsStr) -> case (parseTwoDigit minutesStr, parseTwoDigit secondsStr) of
+                (Right minutes, Right seconds) ->
+                    if minutes <= 59 && seconds <= 59
+                        then Right (minutes, seconds)
+                        else Left "Minutes and seconds must be between 0 and 59"
+                (Left minErr, _) -> Left minErr -- Return the minutes error
+                (_, Left secErr) -> Left secErr -- Return the seconds error
+            _ -> Left "Invalid length format (MM:SS)" -- Correct error message for wrong separator
+
+        parseSS inputStr = case parseTwoDigit inputStr of
+            Right seconds -> if seconds <= 59 then Right (0, seconds) else Left "Seconds must be between 0 and 59"
+            Left err -> Left err
+    in orElse (parseMMSS str) (parseSS str)
+
+parseQuotedString :: String -> Either String String
+parseQuotedString str = case string "\"" str of
+    Left err -> Left err
+    Right _ -> case break (== '"') (drop 1 str) of
+        (content, '"':_) -> Right content
+        _ -> Left "Unterminated quoted string"
+
+skipSpaces :: String -> Either String String
+skipSpaces str = Right (drop (length (takeWhile isSpace str)) str)
+
+parseSong :: String -> Either String Song
+parseSong str = case skipSpaces str of
+    Left err -> Left err
+    Right rest -> case parseQuotedString rest of
+        Left err -> Left err
+        Right title -> case skipSpaces (drop (length ('"' : title ++ "\"")) rest) of
+            Left err -> Left err
+            Right restAfterTitle -> case parseQuotedString restAfterTitle of
+                Left err -> Left err
+                Right singer -> case skipSpaces (drop (length ('"' : singer ++ "\"")) restAfterTitle) of
+                    Left err -> Left err
+                    Right restAfterSinger -> case parseQuotedString restAfterSinger of
+                        Left err -> Left err
+                        Right genre -> case skipSpaces (drop (length ('"' : genre ++ "\"")) restAfterSinger) of
+                            Left err -> Left err
+                            Right restAfterGenre -> case parseLength (dropWhile isSpace restAfterGenre) of
+                                Left err -> Left err
+                                Right len -> Right Song { songTitle = title, songSinger = singer, songGenre = genre, songLength = len }
+
+parseListBy :: (String -> Either String a) -> Char -> String -> Either String [a]
+parseListBy _ _ "" = Right []
+parseListBy parser delimiter str = case break (== delimiter) str of
+    (line, rest) -> case parser line of
+        Left err -> Left err
+        Right item -> case rest of
+            "" -> Right [item]
+            (_:remainingRest) -> parseListBy parser delimiter remainingRest
+
+parseSongList :: String -> Either String [Song]
+parseSongList str = parseListBy parseSong '\n' str
+
 parseQuery :: String -> Either String Query
-parseQuery _ = Left "Not implemented 2"
+parseQuery str = case skipSpaces str of
+    Left initialErr -> Left initialErr
+    Right rest ->
+        case string "OPEN" rest of
+            Right _ -> case skipSpaces (drop (length "OPEN") rest) of
+                Right restAfterOpen -> case parseSongList restAfterOpen of
+                    Right songList -> Right (OpenSongList songList)
+                    Left err -> Left err
+                Left openErr -> Left ("Invalid OPEN command format: " ++ show openErr)
+            Left _ ->  -- If OPEN doesn't match
+                case string "LIKE" rest of
+                    Right _ -> case skipSpaces (drop (length "LIKE") rest) of
+                        Right restAfterLike -> case parseSong (dropWhile isSpace restAfterLike) of
+                            Right song -> Right (LikeSong song)
+                            Left likeParseErr -> Left ("Invalid LIKE command format: " ++ show likeParseErr)
+                        Left likeErr -> Left ("Invalid LIKE command format: " ++ show likeErr)
+                    Left _ -> -- If LIKE doesn't match
+                        case string "UNLIKE" rest of
+                            Right _ -> case skipSpaces (drop (length "UNLIKE") rest) of
+                                Right restAfterUnlike -> case parseSong restAfterUnlike of
+                                    Right song -> Right (UnlikeSong song)
+                                    Left unlikeParseErr -> Left ("Invalid UNLIKE command format: " ++ show unlikeParseErr)
+                                Left unlikeErr -> Left ("Invalid UNLIKE command format: " ++ show unlikeErr)
+                            Left _ -> Left "Invalid command" -- If none match
 
--- | An entity which represents your program's state.
--- Currently it has no constructors but you can introduce
--- as many as needed.
-data State
+-- Program state data type
+data State = State
+    { songs :: [Song] }
+    deriving (Show, Eq)
 
--- | Creates an initial program's state.
--- It is called once when the program starts.
+-- Create an empty initial state
 emptyState :: State
-emptyState = error "Not implemented 1"
+emptyState = State {songs = []}
 
--- | Updates a state according to a query.
--- This allows your program to share the state
--- between repl iterations.
--- Right contains an optional message to print and
--- an updated program's state.
+-- Update program state based on user command
 stateTransition :: State -> Query -> Either String (Maybe String, State)
-stateTransition _ _ = Left "Not implemented 3"
+stateTransition st query = case query of
+    OpenSongList _ -> Right (Just (show (songs st)), st)
+    LikeSong song -> Right (Just "Song liked!", State {songs = song : songs st})
+    UnlikeSong song ->
+        if song `elem` songs st
+            then Right (Just "Song unliked!", State {songs = filter (/= song) (songs st)})
+            else Left "Song not found"
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,16 +2,87 @@
 import Test.Tasty ( TestTree, defaultMain, testGroup )
 import Test.Tasty.HUnit ( testCase, (@?=) )
 
-import Lib1 qualified
+import Lib2 qualified
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [unitTests]
+tests = testGroup "All Tests" [lib2Tests]
 
-unitTests :: TestTree
-unitTests = testGroup "Lib1 tests"
-  [ testCase "List of completions is not empty" $
-      null Lib1.completions @?= False
+lib2Tests :: TestTree
+lib2Tests = testGroup "Lib2 tests"
+  [ testParseQuery
+  , testEmptyState
+  , testStateTransition
+  ]
+
+testParseQuery :: TestTree
+testParseQuery = testGroup "parseQuery tests"
+  [ testCase "Parse LIKE command" $
+      Lib2.parseQuery "LIKE \"Hello world\" \"Singer\" \"Genre\" 03:45" @?=
+        Right (Lib2.LikeSong (Lib2.Song "Hello world" "Singer" "Genre" (3, 45)))
+
+  , testCase "Parse UNLIKE command" $
+      Lib2.parseQuery "UNLIKE \"Hello world\" \"Singer\" \"Genre\" 03:45" @?=
+        Right (Lib2.UnlikeSong (Lib2.Song "Hello world" "Singer" "Genre" (3, 45)))
+
+  , testCase "Parse OPEN command (empty list)" $
+      Lib2.parseQuery "OPEN" @?= Right (Lib2.OpenSongList [])
+
+  , testCase "Parse OPEN command with one song" $
+      Lib2.parseQuery "OPEN \"Song 1\" \"Singer 1\" \"Genre 1\" 03:30" @?= Right (Lib2.OpenSongList [Lib2.Song "Song 1" "Singer 1" "Genre 1" (3,30)])
+
+  , testCase "Parse OPEN command with just spaces after open" $
+      Lib2.parseQuery "OPEN    " @?= Right (Lib2.OpenSongList [])
+
+  , testCase "Parse LIKE command with just spaces after like" $
+      Lib2.parseQuery "LIKE    \"Song 1\" \"Singer 1\" \"Genre 1\" 03:30" @?= Right (Lib2.LikeSong (Lib2.Song "Song 1" "Singer 1" "Genre 1" (3,30)))
+
+  , testCase "Parse UNLIKE command with just spaces after unlike" $
+      Lib2.parseQuery "UNLIKE    \"Song 1\" \"Singer 1\" \"Genre 1\" 03:30" @?= Right (Lib2.UnlikeSong (Lib2.Song "Song 1" "Singer 1" "Genre 1" (3,30)))
+
+    , testCase "Parse song with just seconds" $
+        Lib2.parseQuery "LIKE \"Song 1\" \"Singer 1\" \"Genre 1\" 30" @?= Right (Lib2.LikeSong (Lib2.Song "Song 1" "Singer 1" "Genre 1" (0,30)))
+
+    , testCase "Parse song with just seconds in open" $
+        Lib2.parseQuery "OPEN \"Song 1\" \"Singer 1\" \"Genre 1\" 30" @?= Right (Lib2.OpenSongList [Lib2.Song "Song 1" "Singer 1" "Genre 1" (0,30)])
+        
+    , testCase "Parse song with too many seconds" $
+        Lib2.parseQuery "OPEN \"Song1\" \"Singer 1\" \"Genre 1\" 60" @?= Left "Seconds must be between 0 and 59"
+      ]
+
+testEmptyState :: TestTree
+testEmptyState = testGroup "emptyState tests"
+  [ testCase "Empty state has an empty song list" $
+      Lib2.songs (Lib2.emptyState) @?= []
+  ]
+
+testStateTransition :: TestTree
+testStateTransition = testGroup "stateTransition tests"
+  [ testCase "Like a song" $
+      let song = Lib2.Song "Hello world" "Singer" "Genre" (3, 45)
+          expectedState = Lib2.State {Lib2.songs = [song]}
+      in Lib2.stateTransition Lib2.emptyState (Lib2.LikeSong song) @?= Right (Just "Song liked!", expectedState)
+
+  , testCase "Unlike a liked song" $
+      let song = Lib2.Song "Hello world" "Singer" "Genre" (3, 45)
+          initialState = Lib2.State [song]
+          expectedState = Lib2.State []
+      in Lib2.stateTransition initialState (Lib2.UnlikeSong song) @?= Right (Just "Song unliked!", expectedState)
+
+  , testCase "Unlike a non-existent song" $
+      let song = Lib2.Song "Hello world" "Singer" "Genre" (3, 55)
+      in Lib2.stateTransition Lib2.emptyState (Lib2.UnlikeSong song) @?= Left "Song not found"
+
+    , testCase "Open after liking a song" $
+        let song = Lib2.Song "Hello world" "Singer" "Genre" (3, 45)
+            initialState = Lib2.emptyState
+            expectedOutput = show [song]
+        in case Lib2.stateTransition initialState (Lib2.LikeSong song) of
+            Right (_, stateAfterLike) -> case Lib2.stateTransition stateAfterLike (Lib2.OpenSongList []) of
+                Right (Just message, _) -> message @?= expectedOutput
+                Left err -> fail $ "Open command failed: " ++ err
+                Right (Nothing,_) -> fail "Open command returned Nothing message"
+            Left err -> fail $ "Like command failed: " ++ err
   ]
